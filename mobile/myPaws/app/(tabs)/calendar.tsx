00c5e44ce1ce68464d43150286bb221fc18887da
Swagtiago29 <sant29ramos@gmail.com>
2026-01-27 14:50:25 -0300

    add day tab show

--- mobile/myPaws/app/(tabs)/calendar.tsx
+++ mobile/myPaws/app/(tabs)/calendar.tsx
@@ -1,8 +1,32 @@
-import { useState, useMemo } from "react";
-import { View, Text, Pressable } from "react-native";
+import { useState } from "react";
+import { View, Text, Pressable, FlatList } from "react-native";
 import { Calendar } from 'react-native-big-calendar'
 import IconButton from "../components/IconButton";
 import { generateWeekSlots } from "../utils/generateEvents";
+import { Ionicons } from "@expo/vector-icons";
+
+const DATA: string[] = [
+    "09:00",
+    "09:30",
+    "10:00",
+    "10:30",
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "14:30",
+    "15:00",
+    "15:30",
+    "16:00",
+    "16:30",
+    "17:00",
+    "17:30",
+    "18:00",
+    "18:30",
+];
 
 export default function MyCalendar() {
     const [currentDate, setCurrentDate] = useState(new Date());
@@ -22,17 +46,38 @@ export default function MyCalendar() {
         setCurrentDate(d);
     };
 
+    const formatSpanishDay = (date: Date) =>
+        new Intl.DateTimeFormat("es-UY", {
+            weekday: "long",
+            day: "numeric",
+        }).format(date);
+
+    const capitalize = (text: string) =>
+        text.charAt(0).toUpperCase() + text.slice(1);
+
     return (
-        <View style={{ height: "100%", borderColor: "black", borderTopWidth: 2, borderBottomWidth: 2 }}>
-            {dayOpen && (<Pressable onPress={() => setDayOpen(false)} style={{ position: "absolute", height: "100%", width: "100%", backgroundColor: "black", zIndex: 1 }}>
-                <View>
-                    <Text style={{ color: 'white' }}>
-                        {selectedDate?.toString()}
-                    </Text>
+        <View style={{ height: "100%" }}>
+            {dayOpen && (<Pressable onPress={() => setDayOpen(false)} style={{ position: "absolute", height: "98%", width: "85%", backgroundColor: "white", zIndex: 1, borderRadius: 8, borderWidth: 2, borderColor: '#5C9E3F' }}>
+                <View style={{ padding: 10 }}>
+                    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={{ fontSize: 22, fontWeight: 500 }}>
+                            {selectedDate && capitalize(formatSpanishDay(selectedDate))}
+                        </Text>
+                        <Ionicons name={"close"} size={30} color="black" />
+                    </View>
+
+                    <FlatList
+                        data={DATA}
+                        renderItem={({ item }) => <Text style={{ fontSize: 17, margin: 5 }}>{item}</Text>}>
+
+                    </FlatList>
                 </View>
             </Pressable>)}
+            <View style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between', backgroundColor: '#5C9E3F' }}>
+                <IconButton text="Previous Month" icon="chevron-back" onPress={goPrevMonth} direction={"left"} />
+                <IconButton text="Next Month" icon="chevron-forward" onPress={goNextMonth} direction={"right"} />
+            </View>
 
-            <IconButton text="Previous Month" icon="chevron-back" onPress={goPrevMonth} direction={"left"} />
             <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "400", margin: 10 }}>
                 {currentDate.toLocaleDateString("esp-US", {
                     month: "long",
@@ -65,7 +110,7 @@ export default function MyCalendar() {
                     setDayOpen(true);
                 }}
             />
-            <IconButton text="Next Month" icon="chevron-forward" onPress={goNextMonth} direction={"right"} />
+
         </View>
     );
 }
